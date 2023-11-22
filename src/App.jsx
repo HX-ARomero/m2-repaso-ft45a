457497d78b74e269b2617b6068d8bb9221a4ccd8
AlmafuerteAriel <almafuerte.ariel.1978@gmail.com>
2023-11-22 16:22:21 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+		<div>
+			<h1>Repaso Módulo 2</h1>
+			<hr />
+			<h2>Cohorte FT45a</h2>
+			<h2>Rick & Morty - Episodes</h2>
+			<h3>Ver README.md para ver detalles del proyecto</h3>
+		</div>
+  )
+}
+
+export default App
